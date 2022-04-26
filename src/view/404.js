4f5b5c9d87fbe51094cee907298e35a39470212c
PauRoss <paularosales92@gmail.com>
2022-04-26 10:41:53 -0400

    arreglos css footer y otros

--- src/view/404.js
+++ src/view/404.js
@@ -1,8 +1,11 @@
 export default () => {
   const viewDifferent = `
-    <h2>404</h2>
+  <div class= 'errorView'>
+    <h1>404</h1>
     <h1>Página no encontrada</h1>
-    <p>El archivo específico no se encontró en este sitio web. Por favor compruebe la url
+    <p>El archivo específico no se encontró en este sitio web. 
+    <p>Por favor compruebe la url
+    </div>
     `;
   const divElement = document.createElement('div');
   divElement.setAttribute('id', 'message');
